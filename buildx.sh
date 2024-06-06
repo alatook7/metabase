e33c4782ee5ac86ad736bbc512a6f14b8427c492
seven <alatook@gmail.com>
2024-06-06 15:04:31 +0800

    v0.49.14

--- buildx.sh
+++ buildx.sh
@@ -1,2 +1,2 @@
-ver=$1
+ver=v0.49.14
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag alatook1981/metabase:${ver} --build-arg metabase_repo=metabase --build-arg metabase_version=${ver} .
